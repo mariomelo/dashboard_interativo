3c1462e81e981f87a976e64616fb8c329d16b659
Mário Melo <mariofdemelo@gmail.com>
2014-04-09 15:57:52 -0300

    Cria as widgets dinamicamente

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
   console.log("Iniciou");
-  
+  insereWidgets();
 });
+
+function insereWidgets(){
+  for(var i = 0; i < dashboard_data.length; i++){
+    $("#widgetContainer").append(
+      '<div class="col-lg-'+ dashboard_data[i].size + '">\
+       <div class="panel panel-'+ dashboard_data[i].type + '">\
+         <div class="panel-heading">\
+          <h3 class="panel-title">' + dashboard_data[i].title + '</h3>\
+          </div>\
+          <div class="panel-body">\
+            Panel content\
+          </div>\
+        </div>\
+      </div>'
+    );
+  }
+}
---
+++ dashboard_data.js
@@ -0,0 +1,22 @@
+dashboard_data = [
+  {
+    size: 6,
+    type: 'info',
+    url: 'http://google.com',
+    title: 'Pesquisa rapida'
+  },
+  {
+    size: 6,
+    type: 'success',
+    url: 'http://factivity.herokuapp.com',
+    title: 'Lancamento de Horas'
+  },
+  {
+    size: 12,
+    type: 'primary',
+    url: 'http://www.sotreq.com.br',
+    title: 'Meu cliente favorito'
+  }
+
+
+];
